b815a34668b4d85b90edf08cf248c386f55ffc9a
arpit15006 <arpit6814@gmail.com>
2025-12-04 11:22:23 +0530

    Initial Commit

---
+++ MyDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyDoList
+//
+//  Created by Arpit Patel on 04/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyDoList/MyDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDoListApp.swift
+//  MyDoList
+//
+//  Created by Arpit Patel on 04/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
